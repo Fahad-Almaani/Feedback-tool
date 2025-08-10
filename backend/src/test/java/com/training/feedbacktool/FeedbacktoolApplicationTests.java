d7ec616cf76a3d6085cb1282d519d3fd4332571c
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2025-08-10 17:10:51 +0400

    Initializing the project enviroment

---
+++ backend/src/test/java/com/training/feedbacktool/FeedbacktoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.feedbacktool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbacktoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
