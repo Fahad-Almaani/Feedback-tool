b4fe1288bfabfcb1f4b7e7b05e48742142a2cdd4
Mohammed Al Maskari <maskari2005@gmail.com>
2025-08-14 19:20:48 +0400

    Added columns for user entity

--- backend/src/main/java/com/training/feedbacktool/entity/User.java
+++ backend/src/main/java/com/training/feedbacktool/entity/User.java
@@ -1,11 +1,17 @@
 package com.training.feedbacktool.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import java.time.Instant;
 
 @Entity
 public class User {
     @Id
-    long id;
-
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Email, password_hash, name, role;
+    private Instant created_at;
 }
