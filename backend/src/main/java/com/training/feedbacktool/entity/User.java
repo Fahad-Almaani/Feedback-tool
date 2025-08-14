68e099f8e967cefc47d53edecfe53ce9a72f1bde
Mohammed Al Maskari <maskari2005@gmail.com>
2025-08-14 19:46:34 +0400

    Added constructors, getters, and setters for the user entity

--- backend/src/main/java/com/training/feedbacktool/entity/User.java
+++ backend/src/main/java/com/training/feedbacktool/entity/User.java
@@ -12,6 +12,31 @@ public class User {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
-    private String Email, password_hash, name, role;
+    private String email, password_hash, name, role;
     private Instant created_at;
+
+
+    public User() {
+        this.created_at = Instant.now();
+    }
+
+    public User(String email, String password_hash, String name, String role) {
+        this.email = email;
+        this.password_hash = password_hash;
+        this.name = name;
+        this.role = role;
+        this.created_at = Instant.now();
+    }
+
+    public Long getId() { return id; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword_hash() { return password_hash; }
+    public void setPassword_hash(String password_hash) { this.password_hash = password_hash; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+    public Instant getCreated_at() { return created_at; }
+    public void setCreated_at(Instant created_at) { this.created_at = created_at; }
 }
